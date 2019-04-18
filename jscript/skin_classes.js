3cf8627ba783b52e087c2f7381f36721cd650d91
Pavan Koushik <pavankoushik22@gmail.com>
2019-04-18 16:43:23 +0530

    m7

--- jscript/skin_classes.js
+++ jscript/skin_classes.js
@@ -1,10 +1,10 @@
 SKIN_CLASSES = {
-  0: 'akiec, Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowen’s disease)',
-  1: 'bcc, Basal Cell Carcinoma',
-  2: 'bkl, Benign Keratosis',
-  3: 'df, Dermatofibroma',
-  4: 'mel, Melanoma',
-  5: 'nv, Melanocytic Nevi',
-  6: 'vasc, Vascular skin lesion'
+  0: 'Actinic Keratoses or Carcinoma (akiec)',
+  1: 'Basal Cell Carcinoma (bcc)',
+  2: 'Benign Keratosis (bkl)',
+  3: 'Dermatofibroma (df)',
+  4: 'Melanoma (mel)',
+  5: 'Melanocytic Nevi (nv)',
+  6: 'Vascular skin lesion (vasc)')
 
 };
