fb60a2057523ed5707e6f1a0c9b501d531d941ab
Pavan Koushik <pavankoushik22@gmail.com>
2019-04-18 15:48:03 +0530

    m4

--- jscript/predict.js
+++ jscript/predict.js
@@ -37,7 +37,7 @@ $("#image-selector").change(function () {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('http://skin.test.woza.work/final_model_kaggle_version1/model.json');
+	model = await tf.loadModel('jscript/model.json');
 	model.sumary();
 	$("#selected-image").attr("src", "assets/samplepic.jpg")
 	
